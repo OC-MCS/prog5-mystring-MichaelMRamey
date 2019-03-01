ee123f1848b5f263f9354d10b692b5f557ae0a0b
Michael Ramey <michael.ramey@eagles.oc.edu>
2019-03-01 07:27:39 -0500

    Bug documentation in testMyString.cpp .  Same problem as before where returning a myString causes the program to crash inside the copy constructor

--- MyString/testMyString.cpp
+++ MyString/testMyString.cpp
@@ -3,6 +3,9 @@
 //	3/01/19
 //	Assignment 5: MyString
 //===========================================
+// Known bug: Returning a myString from a function does not work.
+//			It calls the copy constructor like it is supposed to but then it
+//			crashes when the new char array is having the data copied over.
 
 #include <iostream>
 #include "MyString.h"
@@ -13,7 +16,7 @@ int main()
 {
 	myString s1("hello"), s2 = " world", s3; // test vars for mystring
 	s3 = "hello";
-	s3 = s1 + s2;
+	//s3 = s1 + s2;  //There is an issue with adding myStrings
 	s1 = s1;
 	cout << s1;
 	if (s1 == s2)
